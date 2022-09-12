caab31763bce49b9a5af9e83850c7bd764f8fd57
piesieomane <piesieyeboah@gmail.com>
2022-09-12 20:34:20 +0300

    Add screentshots and queries

--- queries.sql
+++ queries.sql
@@ -2,3 +2,6 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND 2019;
+SELECT name FROM animals WHERE neutered AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
