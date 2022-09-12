e97970c5809ca329e85190f9d65564bc16a03734
piesieomane <piesieyeboah@gmail.com>
2022-09-12 20:37:07 +0300

    Add queires for the rest

--- queries.sql
+++ queries.sql
@@ -5,3 +5,6 @@ SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND
 SELECT name FROM animals WHERE neutered AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
